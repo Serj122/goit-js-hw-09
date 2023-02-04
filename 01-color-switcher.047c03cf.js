163a3101e7f0fb282f81454afb79ed07222dbb64
Serj122 <111177082+Serj122@users.noreply.github.com>
2023-02-04 12:33:19 +0000

    Deploying to gh-pages from @ Serj122/goit-js-hw-09@e4c86189cca552ad3635977b99a74214ec238702 🚀

---
+++ 01-color-switcher.047c03cf.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");e.disabled=!0;const i={intervalId:null,isActive:!1,start(){this.isActive||(this.isActive=!0,t.disabled=!0,e.disabled=!1,this.intervalId=setInterval((()=>{document.body.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3))},stop(){clearInterval(this.intervalId),this.isActive=!1,t.disabled=!1,e.disabled=!0}};t.addEventListener("click",i.start.bind(i)),e.addEventListener("click",i.stop.bind(i));
+//# sourceMappingURL=01-color-switcher.047c03cf.js.map
